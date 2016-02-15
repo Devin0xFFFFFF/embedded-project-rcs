98583115de782a3d0e8e1a4a6d98da054bf54b84
Yang Su <yangsu.personal@gmail.com>
2016-02-14 21:02:04 -0600

    refactored using pycharm

--- test_server/flaskserver.py
+++ test_server/flaskserver.py
@@ -1,23 +1,24 @@
 from flask import Flask, request
+
 app = Flask(__name__)
 msgs = []
 
-@app.route('/',methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def hello_world():
-	if request.method == 'POST':
-		msgs.append(request.data)
-		print(request.data)
-		
-	return get_req()
+    if request.method == 'POST':
+        msgs.append(request.data)
+        print(request.data)
+
+    return get_req()
+
 
 def get_req():
-	msg = '<h1>Messages:</h1><ul>'
-	for m in msgs:
-		msg = msg + '<li>{0}</li>'.format(m)
-	msg = msg + '</ul>'
-	return msg
-	
-	
+    msg = '<h1>Messages:</h1><ul>'
+    for m in msgs:
+        msg = msg + '<li>{0}</li>'.format(m)
+    msg = msg + '</ul>'
+    return msg
 
 
 if __name__ == '__main__':
